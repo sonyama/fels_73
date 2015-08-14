e29b2cada8741c2962b532282589b038cbb5ebe9
sonyama <sonyama91@gmail.com>
2015-08-14 14:57:59 +0700

    Word_search

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,7 +1,18 @@
 class WordsController < ApplicationController
 
   def index
-    @words = Word.paginate(page: params[:page])
+    filter_word
   end
 
+  private
+  def filter_word
+    @categories = Category.all
+    if params[:filter_state]
+      @words = Word.paginate page: params[:page], per_page: Settings.paginate_per_page
+      #@words = Word.in_category(params[:category_id]).send(params[:filter_state], current_user)
+      #  .paginate page: params[:page], per_page: Settings.paginate_per_page
+    else
+      @words = Word.paginate page: params[:page], per_page: Settings.paginate_per_page
+    end
+  end
 end
